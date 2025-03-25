9214c4b6538ee752e0227dd42331dc2a364c519b
cheshtapruthi <142924131+cheshtapruthi@users.noreply.github.com>
2025-03-25 21:46:45 +0530

    Update static_sandbox_test.sh

--- static_sandbox_test.sh
+++ static_sandbox_test.sh
@@ -5,7 +5,7 @@ die() {
 
 [[ -n "$COVERAGE" ]] && exit 0
 
-BIN=$TEST_SRCDIR/com_google_sandboxed_api/sandboxed_api/sandbox2/examples/static/static_sandbox
+BIN=$static_sandbox
 
 "$BIN" || die 'FAILED: it should have exited with 0'
 
