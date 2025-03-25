e673f711a797939d27db9a9bbb1859732fa1e888
cheshtapruthi <142924131+cheshtapruthi@users.noreply.github.com>
2025-03-25 21:45:00 +0530

    Update static_sandbox.cc

--- static_sandbox.cc
+++ static_sandbox.cc
@@ -30,37 +30,19 @@
 
 std::unique_ptr<sandbox2::Policy> GetPolicy() {
   return sandbox2::PolicyBuilder()
-      // The most frequent syscall should go first in this sequence (to make it
-      // fast).
-      // Allow read() with all arguments.
       .AllowRead()
-      // Allow a preset of syscalls that are known to be used during startup
-      // of static binaries.
       .AllowStaticStartup()
-      // Allow the getpid() syscall.
       .AllowSyscall(__NR_getpid)
 
-      // Examples for AddPolicyOnSyscall:
       .AddPolicyOnSyscall(__NR_write,
                           {
-                              // Load the first argument of write() (= fd)
                               ARG_32(0),
-                              // Allow write(fd=STDOUT)
                               JEQ32(1, ALLOW),
-                              // Allow write(fd=STDERR)
                               JEQ32(2, ALLOW),
-                              // Fall-through for every other case.
-                              // The default action will be KILL if it is not
-                              // explicitly ALLOWed by a following rule.
                           })
-      // write() calls with fd not in (1, 2) will continue evaluating the
-      // policy. This means that other rules might still allow them.
 
-      // Allow the Sandboxee to set the name for better recognition in the
-      // process listing.
       .AllowPrctlSetName()
 
-      // Allow the dynamic loader to mark pages to never allow read-write-exec.
       .AddPolicyOnSyscall(__NR_mprotect,
                           {
                               ARG_32(2),
@@ -70,30 +52,19 @@ std::unique_ptr<sandbox2::Policy> GetPolicy() {
                               JEQ32(PROT_READ | PROT_EXEC, ALLOW),
                           })
 
-      // Allow exit() only with an exit_code of 0.
-      // Explicitly jumping to KILL, thus the following rules can not
-      // override this rule.
       .AddPolicyOnSyscall(
           __NR_exit_group,
           {
-              // Load first argument (exit_code).
               ARG_32(0),
-              // Deny every argument except 0.
               JNE32(0, KILL),
-              // Allow all exit() calls that were not previously forbidden
-              // = exit_code == 0.
               ALLOW,
           })
 
-      // = This won't have any effect as we handled every case of this syscall
-      // in the previous rule.
       .AllowSyscall(__NR_exit_group)
 
       .BlockSyscallsWithErrno(
           {
 #ifdef __NR_access
-              // On Debian, even static binaries check existence of
-              // /etc/ld.so.nohwcap.
               __NR_access,
 #endif
               __NR_faccessat,
@@ -108,32 +79,21 @@ std::unique_ptr<sandbox2::Policy> GetPolicy() {
 }
 
 int main(int argc, char* argv[]) {
-  // This test is incompatible with sanitizers.
-  // The `SKIP_SANITIZERS_AND_COVERAGE` macro won't work for us here since we
-  // need to return something.
   if constexpr (sapi::sanitizers::IsAny()) {
     return EXIT_SUCCESS;
   }
   absl::SetStderrThreshold(absl::LogSeverityAtLeast::kInfo);
   absl::ParseCommandLine(argc, argv);
   absl::InitializeLog();
-
-  // Note: In your own code, use sapi::GetDataDependencyFilePath() instead.
   const std::string path = sapi::internal::GetSapiDataDependencyFilePath(
       "sandbox2/examples/static/static_bin");
   std::vector<std::string> args = {path};
   auto executor = std::make_unique<sandbox2::Executor>(path, args);
 
   executor
-      // Sandboxing is enabled by the sandbox itself. The sandboxed binary is
-      // not aware that it'll be sandboxed.
-      // Note: 'true' is the default setting for this class.
       ->set_enable_sandbox_before_exec(true)
       .limits()
-      // Kill sandboxed processes with a signal (SIGXFSZ) if it writes more than
-      // these many bytes to the file-system.
       ->set_rlimit_fsize(1024 * 1024)
-      // The CPU time limit.
       .set_rlimit_cpu(60)
       .set_walltime_limit(absl::Seconds(30));
 
